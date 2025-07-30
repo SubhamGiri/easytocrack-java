2a565720b791388b233f588af4d073f3117b2fce
Subham Giri <subhamgiri1001@gmail.com>
2025-07-30 21:25:09 +0530

    Initial commit with all project files

--- src/main/java/com/easytocrack/controllers/HomeController.java
+++ src/main/java/com/easytocrack/controllers/HomeController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ public class HomeController{
 	@Autowired
 	HomeServices					 homeServices;
 
+	@GetMapping("/home")
+	public String test()
+	{
+		return "Welcome to EasyToCrack";
+	}
 	@PostMapping("/")
 	public Map<String, Object> defaultPage()
 	{
