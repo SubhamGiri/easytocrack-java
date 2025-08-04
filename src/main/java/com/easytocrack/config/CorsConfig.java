df1d90723d06bfbea9611f41ef1d3ed52ca229fa
Subham Giri <subhamgiri1001@gmail.com>
2025-08-04 22:36:49 +0530

    Fixed directory 5th

--- src/main/java/com/easytocrack/config/CorsConfig.java
+++ src/main/java/com/easytocrack/config/CorsConfig.java
@@ -15,8 +15,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
